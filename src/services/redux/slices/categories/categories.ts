35424a5cc4ef27167847a07cd9780459ae39656b
Anton Lyscov <anton.lyscov@yandex.ru>
2023-09-24 16:58:12 +0300

    feature: add redux-toolkit

--- src/services/redux/slices/categories/categories.ts
+++ src/services/redux/slices/categories/categories.ts
@@ -2,42 +2,42 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { fetchCategory } from './categoriesAPI';
 
 interface IProduct {
-    sku: string,
-    group: string,
-    category: string,
-    subcategory: string,
-    uom: number
+  sku: string;
+  group: string;
+  category: string;
+  subcategory: string;
+  uom: number;
 }
 
 interface CategoriesState {
-    data: IProduct[]
+  data: IProduct[];
 }
 
 export const getCategoryApi = createAsyncThunk(
-	'@@category/category',
-	async (_, { fulfillWithValue, rejectWithValue }) => {
-		try {
-			const response = await fetchCategory();
-			return fulfillWithValue(response);
-		} catch (error: unknown) {
-			return rejectWithValue(error);
-		}
-	}
+  '@@category/category',
+  async (_, { fulfillWithValue, rejectWithValue }) => {
+    try {
+      const response = await fetchCategory();
+      return fulfillWithValue(response);
+    } catch (error: unknown) {
+      return rejectWithValue(error);
+    }
+  },
 );
 
 const initialState: CategoriesState = {
-    data: [],
+  data: [],
 };
 
 const categoriesSlice = createSlice({
-    name: '@@category',
-    initialState,
-    reducers: {},
-    extraReducers: builder => {
-        builder.addCase(getCategoryApi.fulfilled, (state, action) => {
-            state.data = action.payload;
-        });
-    },
+  name: '@@category',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCategoryApi.fulfilled, (state, action) => {
+      state.data = action.payload;
+    });
+  },
 });
 
 export default categoriesSlice.reducer;
