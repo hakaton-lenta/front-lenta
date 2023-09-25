5dea347676f53e9399a7761bc8ed212041c94980
Anton Lyscov <anton.lyscov@yandex.ru>
2023-09-25 19:13:53 +0300

    feature: add Select menu

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,12 +3,16 @@ import { Outlet } from 'react-router-dom';
 
 import Header from '../header/header';
 import Navigation from '../navigation/navigation';
+import Filter from '../filter/filter';
 
 export const Layout: FC = () => {
   return (
     <div className="layout">
       <Header />
-      <Navigation />
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        <Navigation />
+        <Filter />
+      </div>
       <Outlet />
     </div>
   );
