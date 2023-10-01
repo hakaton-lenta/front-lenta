8876195e2eab605994041eb01da570f0d6750bf6
Anton Lyscov <anton.lyscov@yandex.ru>
2023-10-01 16:26:16 +0300

    hotfix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import { Layout } from './pages/layout/layout';
 import { Provider } from 'react-redux';
 import { store } from './services/redux/store';
@@ -26,11 +26,12 @@ const App = () => {
   return (
     <section className="page">
       <Routes>
-        <Route path={ROUTE_HOME} element={<Layout />}></Route>
+        <Route path={ROUTE_HOME} element={<Layout />}>
+          <Route path="/forecast" element={<Forecast />} />
+          <Route path="/statistics" element={<Statistics />} />
+        </Route>
         <Route path={ROUTE_LOGIN} element={<LoginPage />}></Route>
         <Route path={ROUTE_REGISTER} element={<RegisterPage />}></Route>
-        <Route path="/forecast" element={<Forecast />} />
-        <Route path="/statistics" element={<Statistics />} />
         <Route path="*" element={<NotFound404 />}></Route>
       </Routes>
     </section>
