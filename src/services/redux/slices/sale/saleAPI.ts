7c091ce5673c951711aadf7f243be280505f592f
Anton Lyscov <anton.lyscov@yandex.ru>
2023-10-05 18:57:46 +0300

    fix

--- src/services/redux/slices/sale/saleAPI.ts
+++ src/services/redux/slices/sale/saleAPI.ts
@@ -28,7 +28,7 @@ export const fetchSale = (
   const skuIdStr = skuId.join(',');
   const storeIdStr = storeId.join(',');
   return fetchData(
-    `${API_BASE_URL}/sales/store_product_period?sku=${skuIdStr}&start_date=${date}&store=${storeIdStr}&time_delta=${time}'`,
+    `${API_BASE_URL}/sales/store_product_period?sku=${skuIdStr}&start_date=${date}&store=${storeIdStr}&time_delta=${time}`,
     token,
   );
 };
